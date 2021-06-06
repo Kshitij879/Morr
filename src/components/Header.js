b3907fb1a783e3584de8cb1a4e9d25615efac5ae
Kshitij879 <kshitij.gupta_cs18@gla.ac.in>
2021-06-07 01:31:02 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header(){
             <span style={{fontSize:"30px"}}>DEMO STREAMING</span>
             <div className="links">
                 <a href="#">Log In</a>
-                <a href="#" className="button">Start your free trial today</a>
+                <a href="#" className="button">Start your free trial </a>
             </div>  
         </div>
     );
